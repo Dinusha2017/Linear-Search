6aade6272c1750d6a52170c50c20d2ceb9d96799
Dinusha2017 <dinushagammanpila@gmail.com>
2022-09-18 18:47:59 +0530

    Implementation of Linear Search for integer element in an array

---
+++ LinearSearch/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linear Search for elements in array:");
+
+            int[] arr = { 12, 5, 3, 56, 32, 656, 33 };
+
+            Console.WriteLine(string.Join(", ", arr));
+
+            Console.WriteLine("Search for element 56...");
+
+            int result = SearchElement.LinearSearch(arr, 56);
+            SearchElement.PrintResult(result);
+
+            Console.WriteLine("Search for element 88...");
+
+            result = SearchElement.LinearSearch(arr, 88);
+            SearchElement.PrintResult(result);
+        }
+    }
+}
---
+++ LinearSearch/SearchElement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LinearSearch
+{
+    public static class SearchElement
+    {
+        /// <summary>
+        /// Carries out linear, sequential search to find whether an element exists in an integer array.
+        /// </summary>
+        /// <param name="array">Array that needs to be searched</param>
+        /// <param name="searchValue">Value that needs to be searched</param>
+        /// <returns></returns>
+        public static int LinearSearch(int[] array, int searchValue)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == searchValue)
+                {
+                    return i;
+                }
+            }
+
+            // return -1 if value does not exist in the array
+            return -1;
+        }
+
+        /// <summary>
+        /// If element exists, prints index of array where element exists.
+        /// Else, prints -1 to indicate element is not found.
+        /// </summary>
+        /// <param name="result"></param>
+        public static void PrintResult(int result)
+        {
+            if (result == -1)
+            {
+                Console.WriteLine("Element is not found in array.");
+            }
+
+            Console.WriteLine($"Index of element in array: {result}");
+        }
+    }
+}
